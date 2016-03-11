0656090f3d81a59d483782a170a50d1aeaab06d3
zuqiang <zuqiang@juanpi.com>
2016-03-11 15:41:45 +0800

    Signed-off-by: zuqiang <zuqiang@juanpi.com>

--- src/main/java/com/juanpi/judis/config/JudisProperty.java
+++ src/main/java/com/juanpi/judis/config/JudisProperty.java
@@ -1,5 +1,12 @@
 package com.juanpi.judis.config;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  *
  * @author zuqiang
@@ -7,18 +14,63 @@ package com.juanpi.judis.config;
  */
 public class JudisProperty {
 	
-	private String host;
+	private static final Logger LOG = LoggerFactory.getLogger(JudisProperty.class);
 	
-	private int port;
+	private String host = "localhost";
 	
-	private int maxTotal;
+	private int port = 6379;
 	
-	private int minIdle;
+	private int maxTotal = 8;
 	
-	private int maxIdle;
+	private int minIdle = 8;
 	
-	private long maxWaitMillis;
-
+	private int maxIdle = 5;
+	
+	private long maxWaitMillis = -1L;
+	
+	public JudisProperty(){
+		this(true);
+	}
+	
+	/**
+	 * @param auto_load_config 是否自动查找resources下的judis.properties文件，默认自动查找 。
+	 * 			当为false时，需要自己setValue。 
+	 */
+	public JudisProperty(boolean auto_load_config ){
+		if(auto_load_config){
+			try {
+				InputStream in = this.getClass().getResourceAsStream("judis.properties");
+				Properties props = new Properties();
+				props.load(in);
+				setProperties(props);  
+			} catch (IOException e) {
+				LOG.error("Unable to find or load  judis.properties !!! ",e);
+				throw new RuntimeException("Unable to find or load  judis.properties !!!",e);
+			}
+		}
+	}
+	
+	private void setProperties(Properties props){
+		if(props.getProperty("host")!=null){
+			host = props.getProperty("host");
+		}
+		if(props.getProperty("port")!=null){
+			port = Integer.valueOf(props.getProperty("port"));
+		}
+		if(props.getProperty("maxTotal")!=null){
+			maxTotal = Integer.valueOf(props.getProperty("maxTotal"));
+		}
+		if(props.getProperty("minIdle")!=null){
+			minIdle = Integer.valueOf(props.getProperty("minIdle"));
+		}
+		if(props.getProperty("maxIdle")!=null){
+			maxIdle = Integer.valueOf(props.getProperty("maxIdle"));
+		}
+		if(props.getProperty("maxWaitMillis")!=null){
+			maxWaitMillis = Long.valueOf(props.getProperty("maxWaitMillis"));
+		}
+	}
+	
 	public String getHost() {
 		return host;
 	}
